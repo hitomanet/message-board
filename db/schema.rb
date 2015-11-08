c7c978b729ed8161a173732b1ed41cd9718e4230
hitomanet <hitomane.ngs@gmail.com>
2015-11-08 04:57:39 +0000

    Gemfile update for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107085715) do
+ActiveRecord::Schema.define(version: 20151030131851) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
